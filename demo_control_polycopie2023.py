cff97cbeb537ef3ec1f4b5192e2139d7e2d42ed3
Pavec Philippe <philippe.pavec@student-cs.fr>
2023-11-13 15:30:08 +0100

    ajout desc grad v1

--- demo_control_polycopie2023.py
+++ demo_control_polycopie2023.py
@@ -21,6 +21,124 @@ def compute_J_prim(alpha, u, p) :
         for j in range(N) :
             res[i,j]=(-alpha*u[i,j]*p[i,j]).real()
 
+
+def BelongsInteriorDomain(node):
+                if (node < 0):
+                    return 1
+                if node == 3:
+                    print("Robin")
+                    return 2
+                else:
+                    return 0
+
+
+def compute_gradient_descent(chi, grad, domain, mu):
+    """This function makes the gradient descent.
+    This function has to be used before the 'Projected' function that will project
+    the new element onto the admissible space.
+    :param chi: density of absorption define everywhere in the domain
+    :param grad: parametric gradient associated to the problem
+    :param domain: domain of definition of the equations
+    :param mu: step of the descent
+    :type chi: numpy.array((M,N), dtype=float64
+    :type grad: numpy.array((M,N), dtype=float64)
+    :type domain: numpy.array((M,N), dtype=int64)
+    :type mu: float
+    :return chi:
+    :rtype chi: numpy.array((M,N), dtype=float64
+
+    .. warnings also: It is important that the conditions be expressed with an "if",
+            not with an "elif", as some points are neighbours to multiple points
+            of the Robin frontier.
+    """
+
+    (M, N) = numpy.shape(domain)
+    # for i in range(0, M):
+    # 	for j in range(0, N):
+    # 		if domain_omega[i, j] != _env.NODE_ROBIN:
+    # 			chi[i, j] = chi[i, j] - mu * grad[i, j]
+    # # for i in range(0, M):
+    # 	for j in range(0, N):
+    # 		if preprocessing.is_on_boundary(domain[i , j]) == 'BOUNDARY':
+    # 			chi[i,j] = chi[i,j] - mu*grad[i,j]
+    # print(domain,'jesuisla')
+    #chi[50,:] = chi[50,:] - mu*grad[50,:]
+    for i in range(1, M - 1):
+        for j in range(1, N - 1):
+            #print(i,j)
+            #chi[i,j] = chi[i,j] - mu * grad[i,j]
+            a = BelongsInteriorDomain(domain[i + 1, j])
+            b = BelongsInteriorDomain(domain[i - 1, j])
+            c = BelongsInteriorDomain(domain[i, j + 1])
+            d = BelongsInteriorDomain(domain[i, j - 1])
+            if a == 2:
+                print(i+1,j, "-----", "i+1,j")
+                chi[i + 1, j] = chi[i + 1, j] - mu * grad[i, j]
+            if b == 2:
+                print(i - 1, j, "-----", "i - 1, j")
+                chi[i - 1, j] = chi[i - 1, j] - mu * grad[i, j]
+            if c == 2:
+                print(i, j + 1, "-----", "i , j + 1")
+                chi[i, j + 1] = chi[i, j + 1] - mu * grad[i, j]
+            if d == 2:
+                print(i, j - 1, "-----", "i , j - 1")
+                chi[i, j - 1] = chi[i, j - 1] - mu * grad[i,j]
+    return chi
+
+
+def compute_projected(chi, domain, V_obj):
+    """This function performs the projection of $\chi^n - mu*grad
+
+    To perform the optimization, we use a projected gradient algorithm. This
+    function caracterizes the projection of chi onto the admissible space
+    (the space of $L^{infty}$ function which volume is equal to $V_{obj}$ and whose
+    values are located between 0 and 1).
+
+    :param chi: density matrix
+    :param domain: domain of definition of the equations
+    :param V_obj: characterizes the volume constraint
+    :type chi: numpy.array((M,N), dtype=float64)
+    :type domain: numpy.array((M,N), dtype=complex128)
+    :type float: float
+    :return:
+    :rtype:
+    """
+
+    (M, N) = numpy.shape(domain)
+    S = 0
+    for i in range(M):
+        for j in range(N):
+            if domain[i, j] == _env.NODE_ROBIN:
+                S = S + 1
+
+    B = chi.copy()
+    l = 0
+    chi = set2zero(chi, domain)
+
+    V = numpy.sum(numpy.sum(chi)) / S
+    debut = -numpy.max(chi)
+    fin = numpy.max(chi)
+    ecart = fin - debut
+    # We use dichotomy to find a constant such that chi^{n+1}=max(0,min(chi^{n}+l,1)) is an element of the admissible space
+    while ecart > 10 ** -4:
+        # calcul du milieu 
+        l = (debut + fin) / 2
+        for i in range(M):
+            for j in range(N):
+                chi[i, j] = numpy.maximum(0, numpy.minimum(B[i, j] + l, 1))
+        chi = set2zero(chi, domain)
+        V = sum(sum(chi)) / S
+        if V > V_obj:
+            fin = l
+        else:
+            debut = l
+        ecart = fin - debut
+        # print('le volume est', V, 'le volume objectif est', V_obj)
+
+    return chi
+
+
+
 def your_optimization_procedure(domain_omega, spacestep, omega, f, f_dir, f_neu, f_rob,
                            beta_pde, alpha_pde, alpha_dir, beta_neu, beta_rob, alpha_rob,
                            Alpha, mu, chi, V_obj):
@@ -35,7 +153,7 @@ def your_optimization_procedure(domain_omega, spacestep, omega, f, f_dir, f_neu,
 
     k = 0
     (M, N) = numpy.shape(domain_omega)
-    numb_iter = 100
+    numb_iter = 2
     energy = numpy.zeros((numb_iter+1, 1), dtype=numpy.float64)
     while k < numb_iter and mu > 10**(-5):
         print('1. computing solution of Helmholtz problem, i.e., u')
@@ -48,8 +166,15 @@ def your_optimization_procedure(domain_omega, spacestep, omega, f, f_dir, f_neu,
         print('4. computing parametric gradient')
         while ene >= energy[k] and mu > 10 ** -5:
             print('    a. computing gradient descent')
+            chi = compute_gradient_descent(chi, grad, domain, mu) #chi_k+1 sans projection (l=0)
+
             print('    b. computing projected gradient')
+            chi = compute_projected(chi, domain, V_obj)
+            
             print('    c. computing solution of Helmholtz problem, i.e., u')
+            u=processing.solve_helmholtz(domain_omega, spacestep, omega, f, f_dir, f_neu, f_rob, beta_pde, alpha_pde, alpha_dir, beta_neu, beta_rob, alpha_rob)
+            
+
             print('    d. computing objective function, i.e., energy (E)')
             ene = your_compute_objective_function(domain_omega, u, spacestep)
             if bool_a:
