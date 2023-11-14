d32d38d31bf7fe48f799d824cc23258700b5356b
Francois Baptiste <baptiste.francois@student-cs.fr>
2023-11-14 09:08:29 +0100

    merge

--- demo_control_polycopie2023.py
+++ demo_control_polycopie2023.py
@@ -19,7 +19,132 @@ def compute_J_prim(alpha, u, p) :
     res=np.zeros((M,N))
     for i in range(M) :
         for j in range(N) :
-            res[i,j]=np.real(-alpha[i,j]*u[i,j]*p[i,j])
+            res[i,j]=np.real(-alpha*u[i,j]*p[i,j])
+    return res
+
+
+def BelongsInteriorDomain(node):
+                if (node < 0):
+                    return 1
+                if node == 3:
+                    print("Robin")
+                    return 2
+                else:
+                    return 0
+
+
+def compute_gradient_descent(chi, grad, domain, mu):
+    """This function makes the gradient descent.
+    This function has to be used before the 'Projected' function that will project
+    the new element onto the admissible space.
+    :param chi: density of absorption define everywhere in the domain
+    :param grad: parametric gradient associated to the problem
+    :param domain: domain of definition of the equations
+    :param mu: step of the descent
+    :type chi: np.array((M,N), dtype=float64
+    :type grad: np.array((M,N), dtype=float64)
+    :type domain: np.array((M,N), dtype=int64)
+    :type mu: float
+    :return chi:
+    :rtype chi: np.array((M,N), dtype=float64
+
+    .. warnings also: It is important that the conditions be expressed with an "if",
+            not with an "elif", as some points are neighbours to multiple points
+            of the Robin frontier.
+    """
+
+    (M, N) = np.shape(domain)
+    # for i in range(0, M):
+    # 	for j in range(0, N):
+    # 		if domain_omega[i, j] != _env.NODE_ROBIN:
+    # 			chi[i, j] = chi[i, j] - mu * grad[i, j]
+    # # for i in range(0, M):
+    # 	for j in range(0, N):
+    # 		if preprocessing.is_on_boundary(domain[i , j]) == 'BOUNDARY':
+    # 			chi[i,j] = chi[i,j] - mu*grad[i,j]
+    # print(domain,'jesuisla')
+    #chi[50,:] = chi[50,:] - mu*grad[50,:]
+    for i in range(1, M - 1):
+        for j in range(1, N - 1):
+            #print(i,j)
+            #chi[i,j] = chi[i,j] - mu * grad[i,j]
+            a = BelongsInteriorDomain(domain[i + 1, j])
+            b = BelongsInteriorDomain(domain[i - 1, j])
+            c = BelongsInteriorDomain(domain[i, j + 1])
+            d = BelongsInteriorDomain(domain[i, j - 1])
+            if a == 2:
+                print(i+1,j, "-----", "i+1,j")
+
+                chi[i + 1, j] = chi[i + 1, j] - mu * grad[i, j]
+                #print('chi1:',chi[i + 1, j])
+            if b == 2:
+                print(i - 1, j, "-----", "i - 1, j")
+                chi[i - 1, j] = chi[i - 1, j] - mu * grad[i, j]
+                #print('chi2:',chi[i - 1, j])
+            if c == 2:
+                print(i, j + 1, "-----", "i , j + 1")
+                chi[i, j + 1] = chi[i, j + 1] - mu * grad[i, j]
+                #print('chi3:',chi[i, j+1])
+            if d == 2:
+                print(i, j - 1, "-----", "i , j - 1")
+                chi[i, j - 1] = chi[i, j - 1] - mu * grad[i,j]
+                #print('chi4:',chi[i, j-1])
+    print(chi[100])
+    return chi
+
+
+def compute_projected(chi, domain, V_obj):
+    """This function performs the projection of $\chi^n - mu*grad
+
+    To perform the optimization, we use a projected gradient algorithm. This
+    function caracterizes the projection of chi onto the admissible space
+    (the space of $L^{infty}$ function which volume is equal to $V_{obj}$ and whose
+    values are located between 0 and 1).
+
+    :param chi: density matrix
+    :param domain: domain of definition of the equations
+    :param V_obj: characterizes the volume constraint
+    :type chi: np.array((M,N), dtype=float64)
+    :type domain: np.array((M,N), dtype=complex128)
+    :type float: float
+    :return:
+    :rtype:
+    """
+
+    (M, N) = np.shape(domain)
+    S = 0
+    for i in range(M):
+        for j in range(N):
+            if domain[i, j] == _env.NODE_ROBIN:
+                S = S + 1
+
+    B = chi.copy()
+    l = 0
+    chi = processing.set2zero(chi, domain)
+
+    V = np.sum(np.sum(chi)) / S
+    debut = -np.max(chi)
+    fin = np.max(chi)
+    ecart = fin - debut
+    # We use dichotomy to find a constant such that chi^{n+1}=max(0,min(chi^{n}+l,1)) is an element of the admissible space
+    while ecart > 10 ** -4:
+        # calcul du milieu 
+        l = (debut + fin) / 2
+        for i in range(M):
+            for j in range(N):
+                chi[i, j] = np.maximum(0, np.minimum(B[i, j] + l, 1))
+        chi = processing.set2zero(chi, domain)
+        V = sum(sum(chi)) / S
+        if V > V_obj:
+            fin = l
+        else:
+            debut = l
+        ecart = fin - debut
+        # print('le volume est', V, 'le volume objectif est', V_obj)
+
+    return chi
+
+
 
 def your_optimization_procedure(domain_omega, spacestep, omega, f, f_dir, f_neu, f_rob,
                            beta_pde, alpha_pde, alpha_dir, beta_neu, beta_rob, alpha_rob,
@@ -46,7 +171,7 @@ def your_optimization_procedure(domain_omega, spacestep, omega, f, f_dir, f_neu,
         print('3. computing objective function, i.e., energy')
         J=your_compute_objective_function(u)
         energy[k]=J
-        J_prim=compute_J_prim(alpha_rob, u, p)
+        Jprim=compute_J_prim(Alpha, u, p)
         print('4. computing parametric gradient')
         ene=J
         while ene >= energy[k] and mu > 10 ** -5:
@@ -91,6 +216,7 @@ def your_compute_objective_function(u):
             energy+=abs(u[i,j])**2
 
     return energy
+
 
 
 if __name__ == '__main__':
@@ -176,8 +302,6 @@ if __name__ == '__main__':
     chi0 = chi.copy()
     u0 = u.copy()
 
-    print("shape chi0:",chi0.shape)
-    print("shape u0:",np.real(u0[0:N]))
     # ----------------------------------------------------------------------
     # -- Fell free to modify the function call in this cell.
     # ----------------------------------------------------------------------
@@ -191,8 +315,6 @@ if __name__ == '__main__':
     chin = chi.copy()
     un = u.copy()
 
-    print("shape chin:",chin.shape)
-    print("shape ub:",un.shape)
 
     # -- plot chi, u, and energy
     postprocessing._plot_uncontroled_solution(u0, chi0)
